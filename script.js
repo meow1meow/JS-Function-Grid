d97927101c53b42c99a488c72e2d0d412b23bbc1
tabitha <lilytheboss123@gmail.com>
2016-12-19 22:05:29 +0000

    popsicle

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//Write your function calls below this line
+
+function setColor(row, col, color) {
+    var cell = $('#t')[0].rows[row].cells[col];
+    cell.style.backgroundColor = color;
+}
+// Write your code below this };
+setColor(9, 4, "lightgreen");
+setColor(8, 4, "lightgreen");
+setColor(7, 4, "lightgreen");
+setColor(6, 4, "lightblue");
+setColor(6, 3, "lightblue");
+setColor(6, 2, "lightblue");
+setColor(6, 5, "lightblue");
+setColor(6, 6, "lightblue");
+setColor(6, 4, "lightblue");
+setColor(5, 2, "lightblue");
+setColor(5, 4, "lightpink");
+setColor(5, 3, "lightpink");
+setColor(5, 5, "lightpink");
+setColor(5, 6, "lightblue");
+setColor(4, 6, "lightblue");
+setColor(4, 2, "lightblue");
+setColor(4, 3, "lightpink");
+setColor(4, 4, "lightpink");
+setColor(4, 5, "lightpink");
+setColor(3, 5, "lightpink");
+setColor(3, 4, "lightpink");
+setColor(3, 3, "lightpink");
+setColor(3, 2, "lightblue");
+setColor(3, 6, "lightblue");
+setColor(2, 4, "lightpink");
+setColor(2, 3, "lightpink");
+setColor(2, 2, "lightblue");
+setColor(1, 3, "lightblue");
+setColor(1, 2, "lightblue");
+setColor(1, 4, "lightblue");
